e4c1e839957064e492fd013e3d95717aa6771235
Maurits <maurits@ympact-technologies.nl>
2026-01-20 14:02:56 +0100

    .

--- src/WizardServiceProvider.php
+++ src/WizardServiceProvider.php
@@ -3,6 +3,10 @@
 namespace Ympact\Wizard;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\ComponentHookRegistry;
+use Livewire\Livewire;
+
+use function Livewire\on;
 
 class WizardServiceProvider extends ServiceProvider
 {
@@ -23,10 +27,50 @@ class WizardServiceProvider extends ServiceProvider
             __DIR__.'/../resources/views' => resource_path('views/ympact/wizard'),
         ], 'wizard-views');
 
+
+        foreach ([
+            \Ympact\Wizard\Livewire\SupportStepObjects\SupportStepObjects::class,
+        ] as $feature) {
+            $this->addLivewireFeature($feature);
+        }
+
+
         // Register the commands
         $this->bootCommands();
     }
     
+    protected function addLivewireFeature($feature){
+
+        Livewire::componentHook($feature);
+        
+        on('mount', function (...$args) use ($feature) {
+            // in livewire v3 we have $component, $params, $key, $parent
+            // in livewire v4 we have $component, $params, $key, $parent, $attributes
+            // normalize to livewire v4 signature
+            if(count($args) === 4){ 
+                array_push($args, null);
+            };
+
+            [$component, $params, $key, $parent, $attributes] = $args;
+            
+            if (! $feature = ComponentHookRegistry::initializeHook($feature, $component)) {
+                return;
+            }
+
+            $feature->callBoot();
+            $feature->callMount($params, $parent, $attributes);
+        });
+
+        on('hydrate', function ($component, $memo) use ($feature) {
+            if (! $feature = ComponentHookRegistry::initializeHook($feature, $component)) {
+                return;
+            }
+
+            $feature->callBoot();
+            $feature->callHydrate($memo);
+        });
+    }
+
     public function bootCommands()
     {
         if (! $this->app->runningInConsole()) {
